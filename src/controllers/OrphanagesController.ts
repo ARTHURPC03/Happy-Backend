861e6a0943e0e42d444e5e6b2197621faebe100d
ARTHURPC03 <arthurpimetelcordeiro@gmail.com>
2020-10-13 14:43:29 -0300

    Show orphanages & images

--- src/controllers/OrphanagesController.ts
+++ src/controllers/OrphanagesController.ts
@@ -3,6 +3,24 @@ import { getRepository } from 'typeorm'
 import Orphanage from '../models/Orphanage'
 
 export default {
+  async index(request: Request, response: Response) {
+    const orphanagesRepository = getRepository(Orphanage)
+
+    const orphanages = await orphanagesRepository.find()
+
+    return response.json(orphanages)
+  },
+
+  async show(request: Request, response: Response) {
+    const { id } = request.params
+
+    const orphanagesRepository = getRepository(Orphanage)
+
+    const orphanage = await orphanagesRepository.findOneOrFail(id)
+
+    return response.json(orphanage)
+  },
+
   async create(request: Request, response: Response) {
     const {
       name,
@@ -16,6 +34,11 @@ export default {
 
     const orphanagesRepository = getRepository(Orphanage)
 
+    const requestImages = request.files as Express.Multer.File[]
+    const images = requestImages.map(image => {
+      return { path: image.filename }
+    })
+
     const orphanage = orphanagesRepository.create({
       name,
       latitude,
@@ -24,6 +47,7 @@ export default {
       instructions,
       opening_hours,
       open_on_weekends,
+      images,
     })
 
     await orphanagesRepository.save(orphanage)
