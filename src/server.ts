6624cb05d9bc60e5d8ccc6669b4246d7b8ef8fc9
ARTHURPC03 <arthurpimetelcordeiro@gmail.com>
2020-10-13 13:27:03 -0300

    Create Orphanage

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,11 @@
 import express from 'express'
-// import routes from './routes';
+import './database/connection'
+
+import routes from './routes'
 
 const app = express()
+
+app.use(express.json())
+app.use(routes)
 
 app.listen(3333)
